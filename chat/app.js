95c682e33d10bcb334df9fc30eec30964d5c4408
AlokSinghKumar <ALOK743165@GMAIL.COM>
2020-11-05 23:35:01 +0530

    add chat

--- chat/app.js
+++ chat/app.js
@@ -1,66 +1,66 @@
-const path = require('path');
-const express = require('express');
-const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io').listen(server);
+const path = require('path'),
+      express = require('express'),
+      app = express(),
+      server = require('http').Server(app),
+      io = require('socket.io')(server),
+      {v4: uuidv4} = require('uuid');
 
-app.set("view options", {layout: false});
+require('dotenv').config({ path: path.join(__dirname, '.env') });
 
-//sokcets connection
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(response);
 
-const port = process.env.PORT || 3000;
 
-server.listen(port, err => {
-    console.log(err || 'Listening on port ' + port);
+app.get('/', (req, res) => {
+    let roomId = uuidv4();
+    res.redirect(`/${roomId}`)
 });
 
+app.get("/:room", (req, res) => {
+    res.render('../views/room.ejs');
+});
 
-// users = [];
-// connections = [];
+users = [];
 
-// io.sockets.on('connection', function(socket){
-// 	//new connection
+io.on('connection', function(socket) {
+   console.log('A user connected');
+   socket.on('setUsername', function(data) {
+      console.log(data);
+      
+      if(users.indexOf(data) > -1) {
+         socket.emit('userExists', data + ' username is taken! Try some other username.');
+      } else {
+         users.push(data);
+         socket.emit('userSet', {username: data});
+      }
+   });
+   
+   socket.on('msg', function(data) {
+      //Send message to everyone
+      io.sockets.emit('newmsg', data);
+   })
+});
 
-// 	connections.push(socket);
-// 	console.log('Connected: %s sockets connected', connections.length );
+// io.on('connection', socket => {
+//     console.log("user connected");
+//     socket.on('join-room', (roomId, userId) => {
+//         socket.join(roomId);
+//         socket.to(roomId).broadcast.emit("User-connected", userId);
 
-// 	//disconnect
-// 	socket.on('disconnect',function(data)
-// 	{
-// 		// if(socket.username) return;
-// 		users.splice(users.indexOf(socket.username),1);
-// 		updateUsernames();
-// 		connections.splice(connections.indexOf(socket),1);
-// 		console.log('Disconnected: %s sockets connected', connections.length);
-// 	});
+//         socket.on("disconnect", () => {
+//             socket.to(roomId).broadcast.emit("user-disconnected", userId);
+//             console.log("A user disconnected");
+//         })
 
-// 	// send messages
-// 	socket.on('send message', function(data){
-// 	    console.log(data);
-// 		io.sockets.emit('new_message', {msg:data, user: socket.username});
-// 	});
 
-// 	//new user
-// 	socket.on('new user',function(data,callback){
-// 		callback(true);
-// 		socket.username = data;
-// 		users.push(socket.username);
-// 		updateUsernames();
-// 	});
+//     });
+// })
 
-// 	socket.on('show_chat',function(data,callback)
-// 	{
-// 		callback(true);
-// 		console.log(data);
-// 	});
 
-// 	function updateUsernames(){
-// 		io.sockets.emit('get_users',users);
-// 	}
 
-// });
+
+const port = process.env.PORT || 3030
+server.listen(port, err => {
+    console.log(err || "listening on port: " + port);
+});
